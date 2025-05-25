b36d5eb08316314c1ef0ae1babcaac8f5c802986
RecoverDev <recover@list.ru>
2025-05-25 13:09:45 +0300

    Начинаем писать README.MD

--- src/main/java/ru/student/dateconvertor/Command/UnknowCommand.java
+++ src/main/java/ru/student/dateconvertor/Command/UnknowCommand.java
@@ -1,14 +1,27 @@
 package ru.student.dateconvertor.Command;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import ru.student.dateconvertor.BotComponent.ReplyKeyboard;
+import ru.student.dateconvertor.Service.NextOperation;
+import ru.student.dateconvertor.Service.OperationEnum;
 
 @Component
 public class UnknowCommand extends Command {
 
+    @Autowired
+    private ReplyKeyboard replyKeyboard;
+
+    @Autowired
+    private NextOperation nextOperation;
+
     @Override
     public SendMessage process(String text, String  chatId, String userName) {
         SendMessage response = new SendMessage(chatId, userName + ", я не знаю такой команды" );
+        response.setReplyMarkup(replyKeyboard.getMainReplyKeyboard());
+        nextOperation.setOperation(OperationEnum.CHOICE_STYLE);
         return response;
     }
 }
